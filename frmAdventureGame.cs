bd46ffff55d4cf5942dd8b552ccc7f3b4c5c4e34
Melinda Fischer <mfischer@tda.edu.au>
2020-10-24 19:24:07 +1100

    player class and player initaiton

--- frmAdventureGame.cs
+++ frmAdventureGame.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Engine;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,34 @@ namespace MelindaFischerAssessment2
 {
     public partial class frmAdventureGame : Form
     {
+        //adding variable to store player
+        private Player myPlayer;
+
         public frmAdventureGame()
         {
             InitializeComponent();
+
+            myPlayer = new Player();
+
+            myPlayer.currentHitPoints = 10;
+            myPlayer.maximumHitPoints = 10;
+            myPlayer.gold = 20;
+            myPlayer.experiencePoints = 0;
+            myPlayer.level = 1;
+
+            lblHitPoints.Text = myPlayer.currentHitPoints.ToString();
+            lblGold.Text = myPlayer.gold.ToString();
+            lblExperience.Text = myPlayer.experiencePoints.ToString();
+            lblLevel.Text = myPlayer.level.ToString(); 
+        }
+
+
+
+        
+
+        private void frmAdventureGame_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
